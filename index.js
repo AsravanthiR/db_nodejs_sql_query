2270ec46a5b615e1d81ca48f885bf37f6e63aab6
sravanthi <sravanthi.avadanula@gmail.com>
2022-06-28 16:24:06 +0300

    added more queries

--- index.js
+++ index.js
@@ -4,22 +4,19 @@ import pkg from "pg";
 const { Client } = pkg;
 
 //await connectDB();
-async function connectDB(person_id) {
-  const values = [person_id];
+async function connectDB(name, adress) {
+  const values = [name, adress];
   const client = new Client({
     connectionString: process.env.DB_CONNECTIONSTRING,
   });
   await client.connect();
   const res = await client.query(
-    "DELETE FROM certificates WHERE person_id=$1 ",
+    "INSERT INTO customers (name,address) VALUES ($1, $2) RETURNING *",
     values
   );
-  const res1 = await client.query("SELECT * FROM certificates");
   console.table(res.rows);
-
-  console.table(res1.rows);
 
   //res.rows.forEach((person) => console.log(person));
   await client.end();
 }
-connectDB(5);
+connectDB("Bunny", "xyz");
--- links.sh
+++ links.sh
@@ -113,3 +113,51 @@ async function connectDB(person_id) {
   await client.end();
 }
 connectDB(5);
+---------------------------------------------------------------------
+import "dotenv/config";
+import pkg from "pg";
+
+const { Client } = pkg;
+
+//await connectDB();
+async function connectDB() {
+  const client = new Client({
+    connectionString: process.env.DB_CONNECTIONSTRING,
+  });
+  await client.connect();
+  const res = await client.query(
+    "CREATE TABLE customers(name VARCHAR(255), address VARCHAR(255))"
+  );
+
+  console.table(res.rows);
+
+  //res.rows.forEach((person) => console.log(person));
+  await client.end();
+}
+connectDB();
+------------------------------------------------------------------------
+create table
+
+import "dotenv/config";
+import pkg from "pg";
+
+const { Client } = pkg;
+
+//await connectDB();
+async function connectDB(name, adress) {
+  const values = [name, adress];
+  const client = new Client({
+    connectionString: process.env.DB_CONNECTIONSTRING,
+  });
+  await client.connect();
+  const res = await client.query(
+    "INSERT INTO customers (name,address) VALUES ($1, $2) RETURNING *",
+    values
+  );
+  console.table(res.rows);
+
+  //res.rows.forEach((person) => console.log(person));
+  await client.end();
+}
+connectDB("Bunny", "xyz");
+
